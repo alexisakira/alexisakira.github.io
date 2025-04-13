bbc5051af9bdabb2623df740dedf3276f53b3bfe
Alexis Akira Toda <41405729+alexisakira@users.noreply.github.com>
2025-04-13 16:09:08 -0700

    Update app2.py

--- files/app2.py
+++ files/app2.py
@@ -45,7 +45,7 @@ st.markdown('<p class="highlight-text">Developed by:</p>', unsafe_allow_html=Tru
 st.markdown('<p class="list-text">- <a href="https://alexisakira.github.io/">Alexis Akira Toda</a>, Emory University (data analysis)</p>', unsafe_allow_html=True)
 st.markdown('<p class="list-text">- Zachary Etzioni, Emory University (web tool)</p>', unsafe_allow_html=True)
 
-st.markdown('<p class="highlight-text">The prediction is based on the following parameters:</p>', unsafe_allow_html=True)
+st.markdown('<p class="highlight-text">The prediction is based on the following parameters (R-squared 76%):</p>', unsafe_allow_html=True)
 st.markdown('<p class="list-text">- Education</p>', unsafe_allow_html=True)
 st.markdown('<p class="list-text">- Employment</p>', unsafe_allow_html=True)
 st.markdown('<p class="list-text">- Publications</p>', unsafe_allow_html=True)
